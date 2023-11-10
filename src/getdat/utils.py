dc8e3cc06573017a4beb4328dbcbcb65c30ed6fe
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2023-11-10 15:18:54 -0500

    Refactor AnnasBook

--- src/getdat/utils.py
+++ src/getdat/utils.py
@@ -7,28 +7,30 @@ from bs4 import BeautifulSoup
 class AnnasEbook:
     
     _source_dict = {
-            "name": "Anna's Archive",
-            "url": "https://annas-archive.org",
-            "search_page_scrape": {
-                "tag": "a",
+        "name": "Anna's Archive",
+        "url": "https://annas-archive.org",
+        "search_page_scrape": {
+            "tag": "a",
+            "class": (
+                "js-vim-focus custom-a flex items-center "
+                "relative left-[-10px] w-[calc(100%+20px)] px-[10px] "
+                "outline-offset-[-2px] outline-2 rounded-[3px] hover:bg-[#00000011] "
+                "focus:outline"
+            ),
+            "title_container": {
+                "tag": "div",
                 "class": (
-                    "js-vim-focus custom-a flex items-center "
-                    "relative left-[-10px] w-[calc(100%+20px)] px-[10px] "
-                    "outline-offset-[-2px] outline-2 rounded-[3px] hover:bg-[#00000011] "
-                    "focus:outline"
-                ),
-                "title_container": {
-                    "tag": "div",
-                    "class": (
-                        "line-clamp-[2] leading-[1.2] text-[10px] lg:text-xs text-gray-500"
-                    )
-                }
-            },
-            "detail_page_scrape": {
-                "tag": "a",
-                "class": "js-download-link"
+                    "line-clamp-[2] leading-[1.2] text-[10px] lg:text-xs text-gray-500"
+                )
             }
+        },
+        "detail_page_scrape": {
+            "tag": "a",
+            "class": "js-download-link"
         }
+    }
+    _scrape_key = "search_page_scrape"
+    _return_selected_result = True
 
     def __init__(self, q: tuple,  ext: str):
         self.q = q
@@ -53,60 +55,63 @@ class AnnasEbook:
         click.echo("")
         return requests.get(self._get_url(*args, **kwargs))
     
-    def _scrape(self, scrape_key, response):
+    def _scrape(self, response: object) -> dict:
         soup = BeautifulSoup(response.content, 'html.parser')
-        scrape = self._source_info(key=scrape_key)
+        scrape = self._source_info(key=self._scrape_key)
         tag = scrape.get("tag")
         tag_class = scrape.get("class")
-        search_results = dict()
-        match scrape_key:
+        results = dict()
+        match self._scrape_key:
             case "search_page_scrape":
                 title_container = scrape.get("title_container")
                 tag_title_container = title_container.get("tag")
                 class_title_container = title_container.get("class")
                 for idx, el in enumerate(soup.find_all(tag, class_=tag_class)):
                     title = el.find(tag_title_container, class_=class_title_container).string
-                    search_results[str(idx + 1)] = {
+                    results[str(idx + 1)] = {
                         "title": title,
                         "link": el["href"]
                     }
             case "detail_page_scrape":
                 for idx, el in enumerate(soup.find_all(tag, class_=tag_class)):
                     if el.string != 'Bulk torrent downloads':
-                        search_results[str(idx + 1)] = {
+                        results[str(idx + 1)] = {
                             "title": el.string,
                             "link": el["href"]
                         }
-        search_results["0"] = {
+        results["0"] = {
             "title": "Continue In Browser",
             "link": response.url
         }
-        return search_results
+        return results
     
     @staticmethod
-    def _echo_formatted_title(key, title):
-            title_list = title.split(", ", 3)
-            [lang, ext, size, title] = title_list
+    def _echo_formatted_title(key, title_str):
+            title_list = title_str.split(", ", 3)
+            try:
+                [lang, ext, size, title] = title_list
+            except ValueError:
+                return click.echo(f" {key} | Could not be rendered")
             return click.echo(f" {key} | {title} | {ext} | {size} | {lang}")
     
-    def _echo_results(self, search_results, scrape_key) -> bool:
-        are_search_results = True
-        if len(search_results.keys()) == 1:
+    def _echo_results(self, results) -> bool:
+        have_results = True
+        if len(results.keys()) == 1:
             click.echo("No Search Results Found")
-            are_search_results = False
-            return are_search_results
+            have_results = False
+            return have_results
         click.echo(click.style("Search Results", fg="magenta"))
         click.echo(click.style("==============", fg="magenta"))
         click.echo("")
-        for key in search_results.keys():
-            value = search_results.get(key)
+        for key in results.keys():
+            value = results.get(key)
             title= value.get("title")
             if key == "0":
                 click.echo("")
                 click.echo(
                     click.style(f" {key} | {title}", blink=True)
                 )
-            elif scrape_key == "detail_page_scrape":
+            elif self._scrape_key == "detail_page_scrape":
                 if 'Fast Partner Server' in title:
                     click.echo(f" {key} | {title} - (Continue in Browser / Requires Member Login)")
                 elif 'Slow Partner Server' in title:
@@ -117,35 +122,32 @@ class AnnasEbook:
                 self._echo_formatted_title(key, title)
         click.echo("")
         click.echo("")
-        return are_search_results
-
+        return have_results
     
-    def run(self, *args, **kwargs):
-        search_page_response = self._get()
-        search_results = self._scrape("search_page_scrape", search_page_response)
-        are_search_results = self._echo_results(search_results, "search_page_scrape")
-        if are_search_results:
-            value = click.prompt("Select Number", type=click.IntRange(min=0, max=(len(search_results) - 1)))
-            selected_search_result = search_results.get(str(value))
-            value_link = selected_search_result.get("link")
+    def _scrape_page_results(self, *args, **kwargs):
+        response = self._get(*args, **kwargs)
+        results = self._scrape(response)
+        have_results = self._echo_results(results)
+        if have_results:
+            value = click.prompt("Select Number", type=click.IntRange(min=0, max=(len(results) - 1)))
+            selected_result = results.get(str(value))
+            selected_link = selected_result.get("link")
             if value == 0:
-                return open_new_tab(value_link)
-            click.echo("")
-            click.echo(click.style("Selected", fg="magenta"))
-            click.echo("")
-            title = selected_search_result.get("title")
-            self._echo_formatted_title(value, title)
-            click.echo(click.style("==============", fg="magenta"))
-            kwargs["uri"] = value_link
-            kwargs["msg"] = " for download links..."
-            detail_page_response = self._get(**kwargs)
-            dl_links_results = self._scrape("detail_page_scrape", detail_page_response)
-            are_dl_links_results = self._echo_results(dl_links_results, "detail_page_scrape")
-            if are_dl_links_results:
-                value = click.prompt("Select Number", type=click.IntRange(min=0, max=(len(dl_links_results) - 1)))
-                selected_dl_result = dl_links_results.get(str(value))
-                value_link = selected_dl_result.get("link")
-                if value == 0:
-                    return open_new_tab(value_link)
-            
+                return open_new_tab(selected_link)
+            if self._return_selected_result:
+                return [value, selected_result]
+
+    def run(self, *args, **kwargs):
+        search_page_value, selected_result = self._scrape_page_results(*args, **kwargs)
+        click.echo("")
+        click.echo(click.style("Selected", fg="magenta"))
+        click.echo("")
+        self._echo_formatted_title(search_page_value, selected_result.get("title"))
+        self._scrape_key = "detail_page_scrape"
+        self._return_selected_result = False
+        click.echo(click.style("==============", fg="magenta"))
+        kwargs["uri"] = selected_result.get("link")
+        kwargs["msg"] = " for download links..."
+        self._scrape_page_results(*args, **kwargs)
+        
     
