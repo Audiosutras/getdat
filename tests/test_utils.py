138742d3965cba73b400285810e822531f9a557e
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2023-11-12 21:08:00 -0500

    make sure our error msg is styled and shown

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,6 +7,9 @@ from src.getdat.utils import print_help, AnnasEbook
 class TestPrintHelp:
 
     def test_print_help(self, mocker):
+        spy = mocker.spy(click, "style")
+        msg = "Error msg"
+
         class MockContext:
 
             def get_help(self):
@@ -17,5 +20,10 @@ class TestPrintHelp:
         
         mock_get_current_context = MockContext()
         ctx = mocker.patch.object(click, "get_current_context", return_value=mock_get_current_context)
-        print_help("Yo")
+        print_help(msg)
+        # assert our message is styled with error styling (red).
+        # wrapped in click.echo so it will be shown
+        spy.assert_called_once_with(msg, fg="red")
+        # asserts that the context manager is fetched
+        # to show help text for a command
         ctx.assert_called_once()
