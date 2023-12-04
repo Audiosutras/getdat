d3b68efe472d68c5e542d4d827a55cfbf3e52c34
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2023-12-04 12:02:07 -0500

    update constants

--- src/getdat/constants.py
+++ src/getdat/constants.py
@@ -4,4 +4,4 @@ MOVIE_WEB = "https://movie-web.app"
 
 TOTALSPORTK = "https://www.totalsportk.org/"
 
-BRAINTRUST = "https://bit.ly/braintrust-gigs-talent-search"
+BRAINTRUST = "https://app.usebraintrust.com/r/chris1275/"
